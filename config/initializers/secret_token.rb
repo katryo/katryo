8a4dd20046f1483558199ab628c39e28714dabec
katryo <katoryo55@gmail.com>
2012-05-10 19:53:40 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Katryo::Application.config.secret_token = 'ca70c9de847c05173dec2ec83291682a60dddb1233969cc0638c1d911218641f0aebc2860ed277837a1ca8669d09c118f1df3f9c1a49c64fd28d125f726d8adc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Katryo::Application.config.session_store :cookie_store, key: '_katryo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Katryo::Application.config.session_store :active_record_store
